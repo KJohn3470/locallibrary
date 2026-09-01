8bcea19c82b8c0888bf815ad9eb96981941e03fc
Kalani Johnson <john44kl@dukes.jmu.edu>
2026-09-01 19:05:52 -0400

    Added skeleton framwork

--- locallibrary_config/urls.py
+++ locallibrary_config/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for SkeletonWebsite project.
+URL configuration for locallibrary_config project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/6.1/topics/http/urls/
@@ -16,7 +16,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('catalog/', include('catalog.urls')),
 ]
+
+from django.views.generic import RedirectView
+
+urlpatterns += [
+    path('', RedirectView.as_view(url='catalog/')),
+]
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
