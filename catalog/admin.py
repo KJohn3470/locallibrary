b582c3dc58cadfad0572a6ba746d3fece5c6baf5
Kalani Johnson <john44kl@dukes.jmu.edu>
2026-09-09 13:15:08 -0400

    Challenges for Prep 2

--- catalog/admin.py
+++ catalog/admin.py
@@ -4,7 +4,6 @@ from django.contrib import admin
 from django.contrib import admin
 from .models import Author, Genre, Book, BookInstance, Language
 
-# admin.site.register(Book)
 # Register the Admin classes for Book using the decorator
 class BooksInstanceInline(admin.TabularInline):
     model = BookInstance
@@ -15,21 +14,24 @@ class BookAdmin(admin.ModelAdmin):
 
     inlines = [BooksInstanceInline]
 
-# admin.site.register(Author)
+class BookInline(admin.TabularInline):
+    model = Book
+    
 # Define the admin class
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
 
     fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BookInline]
 
 # Register the admin class with the associated model
 admin.site.register(Author, AuthorAdmin)
 
 admin.site.register(Genre)
-# admin.site.register(BookInstance)
 # Register the Admin classes for BookInstance using the decorator
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'status', 'due_back', 'id')
     list_filter = ('status', 'due_back')
 
     fieldsets = (
